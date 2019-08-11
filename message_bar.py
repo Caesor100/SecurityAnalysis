2ab4f43d546b335c11f5cfaf2e3bd87669213a6a
caesor <HolmesPixel@gmail.com>
2019-08-11 18:23:39 +0300

    Changed length of the dividing line

--- message_bar.py
+++ message_bar.py
@@ -2,7 +2,7 @@ class MessageBar:
     """Class for displaying a message in the information panel."""
     def __init__(self, bar=None):
         self.bar = bar
-        self.dividing_line = '='*40 + '\n'
+        self.dividing_line = '='*20 + '\n'
         self.signal_true = 'command was successful'
 
     def send_message(self, text):
